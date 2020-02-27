dc9d56b66dc21d6ae82fc3c3065150fa78ef657d
BenJokela <benjokela@gmail.com>
2020-02-27 10:55:40 -0400

    scaffold

---
+++ GroceryList/GroceryList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroceryList
+//
+//  Created by Jokela, Benjamin on 2020-02-27.
+//  Copyright © 2020 Jokela, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
